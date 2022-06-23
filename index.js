2e47306e9d4e078bb533449c60ebf4ee9a571270
devthalal <thalal.hassan@neoito.com>
2022-06-23 12:13:06 +0530

    addtodo

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
+import fs from 'fs'
+import path from 'path'
+import { getDB, getBody, sendResponse } from './utils.js'
+import { env } from 'node-blox-sdk'
 
+// Init enviroment
+env.init()
 
+/**
+ * Add todo request hanlder
+ * @param {*} req
+ * @param {*} res
+ */
 const addTodoFunc = async (req, res) => {
+  try {
+    // health check
+    if (req.params['health'] === 'health') {
+      return sendResponse(res, 200, {
+        success: true,
+        msg: 'Health check success',
+      })
+    }
 
-  // health check
-  if (req.params["health"] === "health") {
-    res.write(JSON.stringify({success: true, msg: "Health check success"}))
-    res.end()
+    const DB_FILE = path.resolve(process.env.DB_FILE_PATH)
+    const inmemDB = getDB(DB_FILE)
+    const newId = new Date().getTime()
+    const newItem = await getBody(req)
+    const newEntry = { id: newId, item: newItem }
+    console.log('Request to add -', newItem)
+    inmemDB.push(newEntry)
+    fs.writeFileSync(DB_FILE, JSON.stringify(inmemDB))
+    console.log('Updated DB:\n', inmemDB)
+    console.log('\n')
+    sendResponse(res, 200, newEntry || '[]')
+  } catch (e) {
+    console.log(e)
+    sendResponse(res, 500, { status: 'failed', errMsg: e.message })
   }
-
-  // Add your code here
-  res.write(JSON.stringify({success: true, msg: `Hello addTodoFunc`}))
-  res.end()
-  
 }
 
 export default addTodoFunc
+
